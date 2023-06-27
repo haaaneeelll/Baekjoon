553971f9fa87840b280dcacccd2c5fa53f73f179
Haneul <129492167+haaaneeelll@users.noreply.github.com>
2023-06-28 01:21:32 +0900

    [Bronze V] Title: A+B - 3, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,6 +1,6 @@
-T = int(input())  # 테스트 케이스 개수 입력
+T = int(input())
 
 for _ in range(T):
-    A, B = map(int, input().split())  # A와 B를 입력받음
-    result = A + B  # A와 B의 합을 계산
-    print(result)  # 결과 출력
+    A, B = map(int, input().split())
+    result = A + B
+    print(result)
